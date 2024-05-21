5459263521598bc9a2249e502b84fa7a341c37a3
gaoyvfeng <yufeng.gao@seres.cn>
2024-05-21 17:25:04 +0800

    删除scala 代码

--- server/src/main/java/org/queue/common/InvalidMessageSizeException.scala
+++
@@ -1,9 +0,0 @@
-package org.queue.common
-
-/**
- * Indicates the client has requested a range no longer available on the server
- */
-class InvalidMessageSizeException(message: String) extends RuntimeException(message) {
-  def this() = this(null)
-}
-
